f86976ac51625f252ca3192ea2c82d310f6861a8
hreddel-ux <hreddel@pioix.edu.ar>
2026-08-03 11:36:23 -0300

    arrancuci

---
+++ mi-proyecto-nextjs/src/app/home/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido al Sistema</h1>
+      <p>Esta es la página principal de nuestra aplicación</p>
+    </div>
+  );
+}
